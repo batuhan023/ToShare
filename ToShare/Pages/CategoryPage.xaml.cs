c98571101b843fc42373436f5c2ce6c9d894d15c
Batuhan Yavuz <batuhanyavuz467@gmail.com>
2023-12-27 23:16:44 +0300

    27.12.2023

--- ToShare/Pages/CategoryPage.xaml.cs
+++ ToShare/Pages/CategoryPage.xaml.cs
@@ -1,10 +1,28 @@
+using System.Collections.ObjectModel;
+using ToShare.Models;
+using ToShare.Services;
+
 namespace ToShare.Pages;
 
 public partial class CategoryPage : ContentPage
 {
-	public CategoryPage(int categoryId, string categoryName)
+    private readonly LoginService _login = new LoginService();
+    private readonly PostService _post = new PostService();
+    private readonly CategoryService _category = new CategoryService();
+
+    public ObservableCollection<Product> product { get; set; }
+    public ObservableCollection<Category> category { get; set; }
+    public ObservableCollection<Post> post { get; set; }
+    public CategoryPage(int categoryId, string categoryName)
 	{
 		InitializeComponent();
 		Title = categoryName;
+		GetCategoryList(categoryId);
 	}
+
+    private async void GetCategoryList(int categoryId)
+    {
+		var category = await _category.GetPostsByCategoryId(categoryId);
+        CvCategory.ItemsSource = category;
+    }
 }
---
+++ ToShare/Services/CategoryService.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToShare.Models;
+
+namespace ToShare.Services
+{
+    public class CategoryService
+    {
+        private const string ApiUrl = "https://192.168.1.179:45455/api/";
+
+        private readonly HttpClient _httpClient;
+
+        public CategoryService()
+        {
+            var handler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
+            };
+
+            _httpClient = new HttpClient(handler);
+        }
+
+        public async Task<List<Category>> GetPostsByCategoryId(int id)
+        {
+            var response = await _httpClient.GetStringAsync($"{ApiUrl}Posts/GetPostsByCategoryId?id={id}");
+            return JsonConvert.DeserializeObject<List<Category>>(response);
+        }
+
+    }
+}
