30bc279dd2daa2d609b9b681935e6eb629ca2203
Batuhan Yavuz <batuhanyavuz467@gmail.com>
2024-01-05 15:33:14 +0300

    05.01.2024

--- ToShare/Pages/AddPostPage.xaml.cs
+++ ToShare/Pages/AddPostPage.xaml.cs
@@ -16,8 +16,21 @@ public partial class AddPostPage : ContentPage
         //DateViewModel dateViewModel = (DateViewModel)BindingContext;
         //DateTime selectedDate = dateViewModel.SelectedDate;
         DateTime date = datePicker.Date;
-		int count = int.Parse(Count.Text);
-		int categoryýd = 1;
+        string category = selectionPicker.SelectedItem as string;
+        int categoryýd;
+        if (category == "Food")
+        {
+           categoryýd = 1;
+        } else if (category == "Clothes")
+        {
+            categoryýd = 2;
+        }
+        else
+        {
+            categoryýd = 3;
+        }
+
+        int count = int.Parse(Count.Text);	
 		var userid = Preferences.Get("UserId", 0);
 		var response = await post.AddPost(userid, categoryýd, Name.Text,Adress.Text,count, Description.Text, Image.Text, date);
         if (response != null)
@@ -28,5 +41,6 @@ public partial class AddPostPage : ContentPage
         {
             await DisplayAlert("", "Oops something went wrong", "Cancel");
         }
+        await Navigation.PushModalAsync(new AddPostPage());
     }
 }
